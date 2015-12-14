cb44d19fc7e2876a459ebe57f833b6776d3354dc
Chikara Takeuchi <chikara.takeuchi@gmail.com>
2015-12-14 20:42:09 +0900

    POST周りを追加

--- app/src/main/java/oddtimeworks/com/franken/Activity01.java
+++ app/src/main/java/oddtimeworks/com/franken/Activity01.java
@@ -1,10 +1,21 @@
 package oddtimeworks.com.franken;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.LinearLayout;
 
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -47,5 +58,48 @@ public class Activity01 extends Activity {
         layout = (LinearLayout)findViewById(R.id.screen01);
 
         CreateScreen createScreen = new CreateScreen(num, this, layout, Activity01.this);
+    }
+
+    private void doPost() {
+        String url = "http://jedi.imodeip3.nttdocomo.co.jp/provider/uiux/takeuchi/prototype/filelist.php";
+        String requestJSON = "{'param','dir'}";
+
+        HttpURLConnection conn = null;
+
+        try{
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setFixedLengthStreamingMode(requestJSON.getBytes().length);
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+            conn.connect();
+
+            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+            os.write(requestJSON.getBytes("UTF-8"));
+            os.flush();
+            os.close();
+
+            if(conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                StringBuffer responseJSON = new StringBuffer();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                while((inputLine = reader.readLine()) != null){
+                    responseJSON.append(inputLine);
+                }
+            }
+
+
+        }catch(IOException e) {
+
+        }finally{
+            if(conn != null){
+                conn.disconnect();
+            }
+        }
+
+
+
     }
 }
